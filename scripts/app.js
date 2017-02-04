231a3d7f76b4a18f20186b65a192941e73d2f6f9
Dax <limeside@gmail.com>
2017-02-04 11:49:02 -0600

    Add dialog, passes beer at current index, might switch to the actual object later. :rabbit:

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 angular
     .module('webapp', ['ngMaterial', 'ngMessages', 'ngResource'])
-    .controller('DemoCtrl', function($scope, $http, $resource) {
+    .controller('DemoCtrl', function($scope, $http, $resource, $mdDialog) {
         $scope.beers = []; // If empty, show loading status.
 
 
@@ -11,6 +11,49 @@ angular
           console.log(res.data);
           $scope.beers = res.data;
         });
+
+        $scope.showDialog = function(e, index) {
+          console.log('clicked', index, $scope.beers[index]);
+          $mdDialog.show({
+            locals: {
+              beer: $scope.beers[index]
+            },
+            controller: DialogController,
+            controllerAs:'dialog',
+            // Important note, since I'm not serving these files, I can't use
+            // templateUrl! Otherwise, that would be my go-to option over this. -🐰
+            template:`
+              <md-dialog aria-label="Beer Info">
+                <md-dialog-content>
+                  Testing, {{beer.name}}
+                </md-dialog-content>
+                <md-dialog-actions>
+                  <md-button ng-click="closeDialog()" class="md-primary">Close Dialog</md-button>
+                </md-dialog-actions>
+              </md-dialog>
+
+            ` ,
+            parent: angular.element(document.body),
+            targetEvent: e,
+            clickOutsideToClose:true,
+          })
+        }
+
+        function DialogController($scope, $mdDialog, beer) {
+          $scope.beer = beer;
+
+          $scope.hide = function() {
+            $mdDialog.hide();
+          };
+
+          $scope.cancel = function() {
+            $mdDialog.cancel();
+          };
+
+          $scope.answer = function(answer) {
+            $mdDialog.hide(answer);
+          };
+        }
 
     })
     .config(function($mdThemingProvider) {
