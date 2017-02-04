1481150b16911d996b531a1ccdbb70d970167243
Dax <limeside@gmail.com>
2017-02-04 10:32:44 -0600

    Add basic functionality of UI. :rabbit:

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,15 @@
 angular
     .module('webapp', ['ngMaterial', 'ngMessages', 'ngResource'])
     .controller('DemoCtrl', function($scope, $http, $resource) {
-        var vm = this;
-        
+        $scope.beers = []; // If empty, show loading status.
+
+
         // Do we want to refactor this for the sake of separation of concerns?
         // For scaleability, we might want to put this in a service/facotry
 
         $http.get('https://api.punkapi.com/v2/beers').then(res => {
-          var vm.beers = res.data;
+          console.log(res.data);
+          $scope.beers = res.data;
         });
 
     })
