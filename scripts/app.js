3fa44a9e14cb3cfedb76d99fb8bed3c428cba2ba
Dax <limeside@gmail.com>
2017-02-04 10:13:38 -0600

    Add API call for DemoCtrl. :rabbit:

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,15 @@
 angular
     .module('webapp', ['ngMaterial', 'ngMessages', 'ngResource'])
     .controller('DemoCtrl', function($scope, $http, $resource) {
-
-
-        // use $http or $resource
-        // https://api.punkapi.com/v2/beers
-
+        var vm = this;
         
+        // Do we want to refactor this for the sake of separation of concerns?
+        // For scaleability, we might want to put this in a service/facotry
+
+        $http.get('https://api.punkapi.com/v2/beers').then(res => {
+          var vm.beers = res.data;
+        });
+
     })
     .config(function($mdThemingProvider) {
         $mdThemingProvider.theme('docs-dark', 'default')
